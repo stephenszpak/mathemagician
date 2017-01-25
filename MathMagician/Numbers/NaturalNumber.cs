a82ee4588bb51679b10d58b3661b4d95a036a615
Stephen Szpak <srszpak@gmail.com>
2017-01-24 19:31:32 -0800

    Made GetFirst a constructor

--- MathMagician/Numbers/NaturalNumber.cs
+++ MathMagician/Numbers/NaturalNumber.cs
@@ -8,9 +8,18 @@ namespace MathMagician.Numbers
 {
     public class NaturalNumber : Integer
     {
+        //private makes it where you can't access outside of the class
+        private int First { get; set; }
+
+        //constructors do not have return types and have to be public
+        public NaturalNumber()
+        {
+            this.First = 1;
+        }
+
         public int GetFirst()
         {
-            return 1;
+            return First;
         }
 
         public int GetNext(int current)
